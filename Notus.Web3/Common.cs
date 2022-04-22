27e180761c464f5e7b27ac6b2f9a6f6a8d34a4c4
Omer GOKSOY <42944329+omergoksoy@users.noreply.github.com>
2022-04-23 02:18:33 +0300

    In this release, Common library have been added.

---
+++ Notus.Web3/Common.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Notus.Web3
+{
+    public class Common
+    {
+        public static async Task<string> PostRequest(string UrlAddress, Dictionary<string, string> PostData)
+        {
+            HttpResponseMessage response = await new HttpClient().PostAsync(UrlAddress, new FormUrlEncodedContent(PostData));
+            return await response.Content.ReadAsStringAsync();
+        }
+        public static string MakeHttpListenerPath(string IpAddress, int PortNo, bool UseSSL = false)
+        {
+            return (UseSSL == true ? "https" : "http") + "://" + IpAddress + ":" + PortNo + "/";
+        }
+    }
+}
--- Notus.Web3/dApp.cs
+++ Notus.Web3/dApp.cs
@@ -4,22 +4,14 @@ namespace Notus.Web3
 {
     public class dApp
     {
-        public static bool GenerateToken(string PrivateKeyHex, Notus.Variable.Token.TokenInfoStruct Obj_TokenInfo, Notus.Variable.Token.SupplyStruct Obj_TokenSupply)
+        public static Notus.Variable.Struct.BlockResponseStruct GenerateToken(string PrivateKeyHex, Notus.Variable.Token.TokenInfoStruct Obj_TokenInfo, Notus.Variable.Token.SupplyStruct Obj_TokenSupply)
         {
             //string PublicKeyHex = Notus.Wallet.ID.GetAddressWithPublicKey(PrivateKeyHex);
             string PublicKeyHex = Notus.Wallet.ID.Generate(PrivateKeyHex);
             string TokenRawDataForSignText = Notus.Token.Generate.RawDataForSign(PublicKeyHex, Obj_TokenInfo, Obj_TokenSupply);
             string SignText = Notus.Wallet.ID.Sign(TokenRawDataForSignText, PrivateKeyHex);
-            int TokenResultInt = Notus.Token.Generate.Execute(PublicKeyHex, SignText, Obj_TokenInfo, Obj_TokenSupply);
-            if (TokenResultInt == 0)
-            {
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("TokenResultInt : " + TokenResultInt);
-                return false;
-            }
+            Notus.Variable.Struct.BlockResponseStruct TokenResult = Notus.Token.Generate.Execute(PublicKeyHex, SignText, Obj_TokenInfo, Obj_TokenSupply);
+            return TokenResult;
         }
         public static Notus.Variable.Struct.EccKeyPair GenerateKeyPair()
         {
