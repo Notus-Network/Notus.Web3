cba1bb5074ec429adff1ecad2d9a1e32eeffc2db
Omer GOKSOY <42944329+omergoksoy@users.noreply.github.com>
2022-04-24 03:11:07 +0300

    Airdrop have been added.

--- Notus.Web3/Common.cs
+++ Notus.Web3/Common.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -6,10 +7,83 @@ namespace Notus.Web3
 {
     public class Common
     {
+        public static string FindAvailableNode(string UrlText)
+        {
+            string MainResultStr = string.Empty;
+            bool exitInnerLoop = false;
+            while (exitInnerLoop == false)
+            {
+                for (int a = 0; a < Notus.Variable.Constant.ListMainNodeIp.Count && exitInnerLoop == false; a++)
+                {
+                    try
+                    {
+                        MainResultStr = GetRequest(
+                            MakeHttpListenerPath(
+                                Notus.Variable.Constant.ListMainNodeIp[a],
+                                Notus.Variable.Constant.PortNo_HttpListener
+                            ) + UrlText,
+                            10,
+                            true
+                        );
+                    }
+                    catch (Exception err)
+                    {
+                        SleepWithoutBlocking(5, true);
+                    }
+                    exitInnerLoop = (MainResultStr.Length > 0);
+                }
+            }
+            return MainResultStr;
+        }
         public static async Task<string> PostRequest(string UrlAddress, Dictionary<string, string> PostData)
         {
             HttpResponseMessage response = await new HttpClient().PostAsync(UrlAddress, new FormUrlEncodedContent(PostData));
             return await response.Content.ReadAsStringAsync();
+        }
+        public static string GetRequest(string UrlAddress, int TimeOut = 0, bool UseTimeoutAsSecond = true)
+        {
+            try
+            {
+                System.Net.WebRequest webRequest = System.Net.WebRequest.Create(UrlAddress);
+                if (TimeOut > 0)
+                {
+                    if (UseTimeoutAsSecond == true)
+                    {
+                        webRequest.Timeout = TimeOut * 1000;
+                    }
+                    else
+                    {
+                        webRequest.Timeout = TimeOut;
+                    }
+                }
+                using (System.Net.WebResponse response = webRequest.GetResponse())
+                {
+                    using (System.IO.Stream content = response.GetResponseStream())
+                    {
+                        using (System.IO.StreamReader reader = new System.IO.StreamReader(content))
+                        {
+                            return reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+            return string.Empty;
+        }
+        public static void Sleep(int SleepTime, bool UseAsSecond = false)
+        {
+            SleepWithoutBlocking(SleepTime, UseAsSecond);
+        }
+        public static void SleepWithoutBlocking(int SleepTime, bool UseAsSecond = false)
+        {
+            DateTime NextTime = (UseAsSecond == true ? DateTime.Now.AddMilliseconds(SleepTime) : DateTime.Now.AddSeconds(SleepTime));
+            while (NextTime > DateTime.Now)
+            {
+
+            }
         }
         public static string MakeHttpListenerPath(string IpAddress, int PortNo, bool UseSSL = false)
         {
--- Notus.Web3/dApp.cs
+++ Notus.Web3/dApp.cs
@@ -4,6 +4,13 @@ namespace Notus.Web3
 {
     public class dApp
     {
+        private string Val_PrivateKeyHex;
+        public string PrivateKeyHex
+        {
+            get { return Val_PrivateKeyHex; }
+            set { Val_PrivateKeyHex = value; }
+        }
+
         public static Notus.Variable.Struct.BlockResponseStruct GenerateToken(string PrivateKeyHex, Notus.Variable.Token.TokenInfoStruct Obj_TokenInfo, Notus.Variable.Token.SupplyStruct Obj_TokenSupply)
         {
             //string PublicKeyHex = Notus.Wallet.ID.GetAddressWithPublicKey(PrivateKeyHex);
@@ -12,6 +19,19 @@ namespace Notus.Web3
             string SignText = Notus.Wallet.ID.Sign(TokenRawDataForSignText, PrivateKeyHex);
             Notus.Variable.Struct.BlockResponseStruct TokenResult = Notus.Token.Generate.Execute(PublicKeyHex, SignText, Obj_TokenInfo, Obj_TokenSupply);
             return TokenResult;
+        }
+        public static string Balance(string WalletKey)
+        {
+            string Result = Notus.Web3.Common.FindAvailableNode("balance/" + WalletKey + "/");
+            Console.WriteLine(Result);
+            return Result;
+        }
+        public static string AirDrop(string WalletKey)
+        {
+            string Result=Notus.Web3.Common.FindAvailableNode("airdrop/" + WalletKey + "/");
+            Console.WriteLine(Result);
+            return Result;
+            //WalletKey
         }
         public static Notus.Variable.Struct.EccKeyPair GenerateKeyPair()
         {
